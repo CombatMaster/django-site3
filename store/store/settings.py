8947c213ed4de69d24dc33b2353b679c65780adb
Batyrbekkk <bdiasbekn@gmail.com>
2023-04-29 18:09:55 +0600

    false to False

--- store/store/settings.py
+++ store/store/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(810(=d&prci@i-#an*jfb0v7put@06wqu=-4zr!2%iaq!9c50'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
