c6910bf6572bddd1bbe43c44d33e510a78335fff
CombatMaster <ilalysenko151@gmail.com>
2023-05-31 13:05:30 +0300

    Added HTML and CSS to site, and transfer to urls

--- store/app/urls.py
+++ store/app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("about/", views.about_category),
     path("main/", views.about_category),
     path("man/", views.man_category),
-    path("small_kids", views.small_kids_category),
+    path("small_kids/", views.small_kids_category),
     path("women/", views.women_category)
 ]
--- store/app/views.py
+++ store/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def main(response):
+def main (response):
     return HttpResponse("<h3>Hello World! :) :) :)</h3>")
 # Testing category "about"
 def about_category(response):
